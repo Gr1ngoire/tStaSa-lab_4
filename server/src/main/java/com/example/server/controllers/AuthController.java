12f4cf7b5aa86f7803192e08c96cebf674450540
FilosofDanil <serhio3347@gmail.com>
2024-04-16 22:38:13 +0200

    Auth

--- server/src/main/java/com/example/server/controllers/AuthController.java
+++ server/src/main/java/com/example/server/controllers/AuthController.java
@@ -1,10 +1,39 @@
 package com.example.server.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.server.dtos.AppointmentDTO;
+import com.example.server.dtos.LoginDTO;
+import com.example.server.dtos.RegisterDTO;
+import com.example.server.entities.AppointmentEntity;
+import com.example.server.entities.UserEntity;
+import com.example.server.services.AppointmentService;
+import com.example.server.services.AuthService;
+import com.example.server.services.UserService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/auth")
 public class AuthController {
+    private final AuthService authService;
 
+    private final UserService userService;
+
+    public AuthController(AuthService authService, UserService userService) {
+        this.authService = authService;
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public UserEntity create(@RequestBody RegisterDTO registerDTO){
+        return authService.register(registerDTO);
+    }
+
+    @PostMapping("/login")
+    public Boolean login(@PathVariable LoginDTO loginDTO){
+        return authService.login(loginDTO);
+    }
+
+    @PostMapping("/check")
+    public Boolean check(){
+        return authService.checkAuth();
+    }
 }
---
+++ server/src/main/java/com/example/server/services/AuthService.java
@@ -0,0 +1,36 @@
+package com.example.server.services;
+
+import com.example.server.dtos.LoginDTO;
+import com.example.server.dtos.RegisterDTO;
+import com.example.server.entities.UserEntity;
+import com.example.server.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    private final UserRepository userRepository;
+
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserEntity register(RegisterDTO registerDTO){
+        UserEntity userEntity = new UserEntity(userRepository.getSize(),
+                registerDTO.getEmail(), registerDTO.getPassword(), registerDTO.getName());
+        userRepository.create(userEntity);
+        return userEntity;
+    }
+
+    public Boolean login(LoginDTO loginDTO){
+        if(userRepository.findByEmail(loginDTO.getEmail())
+                .getPassword().equals(loginDTO.getPassword())){
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean checkAuth(){
+        return true;
+    }
+
+}
